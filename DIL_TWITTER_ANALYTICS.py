039000714cf4368ee24726ad20715f402ab7020a
rabi320 <ryonatan6@gmail.com>
2022-08-07 18:06:46 +0300

    A

--- DIL_TWITTER_ANALYTICS.py
+++ DIL_TWITTER_ANALYTICS.py
@@ -144,6 +144,8 @@ if st.button('Results:'):
     most_liked_tweet = day_df[cond2]["tweet"].values[0]
     most_liked_tweet = most_liked_tweet
     st.text(f"{type(most_liked_tweet)} Likes: {day_df['likes_count'].max()}")
+    st.text(f"{most_liked_tweet.encode('UTF-8-sig')}")
+    
     st.dataframe(pd.DataFrame(day_df[cond2]["tweet"]))
     
     
