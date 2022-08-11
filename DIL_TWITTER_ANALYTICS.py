e3391ca6ea5d0636340452b7b28a91773c57cf2f
rabi320 <ryonatan6@gmail.com>
2022-08-11 18:13:05 +0300

    A

--- DIL_TWITTER_ANALYTICS.py
+++ DIL_TWITTER_ANALYTICS.py
@@ -171,7 +171,8 @@ if st.button('Results:'):
         most_liked_replies = day_df[cond2]["replies_count"].values[0]
         most_liked_link = day_df[cond2]["link"].values[0]
         
-        st.text(f"{most_liked_person} - {most_liked_tweet} Likes: {day_df['likes_count'].max()}\n Replies count: {most_liked_replies} Retweet count: {most_liked_retweet} Link: {most_liked_link}")
+        st.text(f"{most_liked_person} - {most_liked_tweet} Likes: {day_df['likes_count'].max()}\n Replies count: {most_liked_replies} Retweet count: {most_liked_retweet}")
+        st.write(f'Link: [link]({most_liked_link})')
         
         #st.text(f"{most_liked_tweet.encode('UTF-8-sig')}")
         #st.dataframe(pd.DataFrame(day_df[cond2]["tweet"]))
