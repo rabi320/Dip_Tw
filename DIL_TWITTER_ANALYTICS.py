ae2be14681aed189522a272c42d7d7931b30d453
rabi320 <ryonatan6@gmail.com>
2022-09-05 14:00:09 +0300

    A

--- DIL_TWITTER_ANALYTICS.py
+++ DIL_TWITTER_ANALYTICS.py
@@ -13,7 +13,7 @@ import re
 from collections import Counter
 from wordcloud import WordCloud
 
-st.markdown("""<img src="Data\Dip.jpg)" alt="drawing" width="200"/>""", unsafe_allow_html=True)
+#st.markdown("""<img src="Data\Dip.jpg)" alt="drawing" width="200"/>""", unsafe_allow_html=True)
 
 
 #with st.spinner('Loading tweets in app...'):
